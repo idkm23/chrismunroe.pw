c514689a7cb3c672ddc8301c53f11af71bc6bce8
idkm23@gmail.com <idkm23@gmail.com>
2015-07-20 23:23:49 -0400

    implemented the object oriented design pulled from codepen

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 var BASE_SPEED = 6, MAX_SPEED = 40;
-var bckDiv = document.getElementById("ballWalls");
-var ball_count = 0;
+var bckDiv = document.getElementById("main");
+var ball_count = 0, BALL_COUNT_MAX = 30;
 
 function Ball() {
-	if(ball_count > 20)
+	if(ball_count > BALL_COUNT_MAX)
 		return;
 	
 	this.id = ball_count;
@@ -12,14 +12,14 @@ function Ball() {
 	this.speed = BASE_SPEED;
 	this.theta = Math.random() * Math.PI * 2;
 	this.div = document.createElement("div" + ball_count++);
-	styleDiv();
+	this.styleDiv();
 }
 
 Ball.prototype.styleDiv = function() {
   	this.div.style.display = "none";
 	this.div.style.height = "100px";
   	this.div.style.width = "100px";
-  	this.div.style.background = "#9c9";
+  	this.div.style.background = "#aaf";
   	this.div.style.borderRadius = "50%";
   	this.div.style.position = "absolute";
   	this.div.style.top = this.x + "px";
@@ -27,59 +27,69 @@ Ball.prototype.styleDiv = function() {
   	this.div.style.cursor = "pointer";
   	this.div.style.zIndex = "-1";
 	this.div.style.display = "block";
+    this.div.style.opacity = ".4";
+	document.body.appendChild(this.div);
 }
 
 function isObstructed(a, b) {
   	return (a < 0 || a + 100 > bckDiv.offsetWidth || b < 0 || b + 100 > bckDiv.offsetHeight);
 }
 
-var step_x, step_y;
+var step_x, step_y, xAndStep, yAndStep;
 Ball.prototype.move = function() {
   	step_x = this.speed * Math.cos(this.theta);
   	step_y = this.speed * Math.sin(this.theta);
-
-	if (!isObstructed(this.x + step_x, y + step_y)) {
+    xAndStep = this.x + step_x;
+    yAndStep = this.y + step_y;
+   
+	if (!isObstructed(xAndStep, yAndStep)) {
 		this.x += step_x;
 		this.y += step_y;
 	} else {
-		if(isObstructed(this.x + step_x, this.y)) {
+		if(isObstructed(xAndStep, this.y)) {
       
       		if((this.theta = Math.PI - this.theta) < 0)
           		this.theta += 2*Math.PI;
 				
 	  		//add the extra to move towards new theta
-	  		if(this.x + step_x < 0)
-	  			this.x = 0;
+	  		if(xAndStep < 0)
+	  			this.x = -xAndStep;
 	  		else
-	  			this.x = bckDiv.offsetWidth - 100; 
+	  			this.x = 2*(bckDiv.offsetWidth - 100) - xAndStep; 
 		
-    	} else {
-			
-        	this.theta = 2*Math.PI - this.theta;
+    	}
+		if(isObstructed(this.x, yAndStep)) {
+				
+			this.theta = 2*Math.PI - this.theta;
 			
 			//have extra count towards new theta
-			if(this.y + step_y < 0)
-				this.y = 0;
+			if(yAndStep < 0)
+				this.y = -yAndStep;
 			else
-				this.y = bckDiv.offsetHeight - 100;
+				this.y = 2*(bckDiv.offsetHeight - 100) - yAndStep;
 			
 		}
 	}
 
-	this.ball.style.left = x + "px";
-	this.ball.style.top = y + "px";
+	this.div.style.left = this.x + "px";
+	this.div.style.top = this.y + "px";
 	if(this.speed > BASE_SPEED) {
 		this.speed -= .2;
 	}
 	
-	//call next ball's move?
 }
 
-move();
-
-/*
-function speedUp() {
-  if(speed < MAX_SPEED)
-    speed = speed * 2;
+//sets up the balls
+var balls = [];
+while(ball_count < BALL_COUNT_MAX) {
+	balls[ball_count] = new Ball();
 }
-*/
+
+function moveBalls() {
+	for(j = 0; j < ball_count; j++) {
+		balls[j].move();
+	}
+	setTimeout(moveBalls, 50);
+}
+
+moveBalls();

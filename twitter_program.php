9617ceaf751ba10b850c1bb189dff4728631bc7f
idkm23 <idkm23@gmail.com>
2015-08-15 13:51:45 -0400

    stable embedded

--- twitter_program.php
+++ twitter_program.php
@@ -1,11 +1,29 @@
 
+<!-- Properly displays embeded tweets -->
+<script>window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));</script>
+
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Twitter Profile Demo using jQuery</title>
 </head>
 <body>
-	
+    
 	<?php require_once('inc/twitter-api-php/TwitterAPIExchange.php');
     
         /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
@@ -16,36 +34,63 @@
             'consumer_secret' => "vX1JAeXs84rH5r9rXYpSWM13aHdtkeoC1ymiRdckm9vlvkYWVw"
         );
         
-        /** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
-        $url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-        $requestMethod = 'GET';
-        $twitter = new TwitterAPIExchange($settings);
+		$screen_name = 'tay_humphreys';
+		$user_id = '343110644';
 		
-		for($page = 0; $page < 16; $page++)
+        /** Sets up tweet_list fetcher **/
+        $list_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+        $list_api = new TwitterAPIExchange($settings);
+		
+		$embedded_api = new TwitterAPIExchange($settings);
+		$embedded_url = 'https://api.twitter.com/1.1/statuses/oembed.json';
+		
+		for($page = 0; $page < 1; $page++)
 		{
-        	$getfield = '?count=200&page=' . $page . '&include_rts=false&user_id=343110644&screen_name=tay_humphreys';
+        	$list_getfield = '?count=200&page=' . $page . '&include_rts=false&user_id=' . $user_id . '&screen_name=' . $screen_name;
         
-			$resulting_json = json_decode(
-							$twitter->setGetfield($getfield)
-                     		->buildOauth($url, $requestMethod)
-                     		->performRequest(),
-							true
-						);  
+			$tweet_list = json_decode(
+				$list_api->setGetfield($list_getfield)
+				->buildOauth($list_url, 'GET')
+				->performRequest(),
+				true
+			);  
 		
 		
-			foreach($resulting_json as &$one_tweet)
+			foreach($tweet_list as &$tweet)
 			{
-	?>	
+	
+    			$embedded_getfield = '?align=left&id=' . $tweet['id_str'] 
+					. '&url=https%3A%2F%2Ftwitter.com%2F' . $screen_name . '%2Fstatus%2F' . $tweet['id_str']
+					. '&omit_script=true';
+				
+				$embedded_tweet = json_decode(
+					$embedded_api->setGetfield($embedded_getfield)
+					->buildOauth($embedded_url, 'GET')
+					->performRequest(),
+					true
+				); 
+				
+				echo $embedded_tweet['html'];
+     		}
+		}
+    ?>
+    
+    
+    
+    </body>
+</html>
+
+<!-- 
     		<div class = "one_tweet_wrap">
                 <div class = "one_tweet">
-                    <p class = "date"><?php echo $one_tweet['created_at']?></p> 
-                    <p class = "tweet_id"><?php echo $one_tweet['id_str']; ?></p>
-                    <p class = "raw_text"><?php echo $one_tweet['text']; ?></p>
+                    <p class = "date"><?php //echo $one_tweet['created_at']?></p> 
+                    <p class = "tweet_id"><?php //echo $one_tweet['id_str']; ?></p>
+                    <p class = "raw_text"><?php //echo $one_tweet['text']; ?></p>
                 </div>
             </div>
 	<?php
-			}
-		}
+		//	}
+	//	}
 	?>
     
     <style>
@@ -57,6 +102,8 @@
 		}
 	
 	</style>
-    
-</body>
-</html>
+
+	Fetch 5 tweets from tay:
+    https://api.twitter.com/1.1/statuses/user_timeline.json?count=5&include_rts=false&user_id=343110644&screen_name=tay_humphreys
+
+-->
